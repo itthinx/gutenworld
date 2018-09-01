8dc0df647563f6fca72bfb671df9cfffcd5f0d40
itthinx <itthinx@gmail.com>
2018-09-01 15:19:32 +0200

    added more example blocks

--- gutenworld.php
+++ gutenworld.php
@@ -51,6 +51,8 @@ class GutenWorld {
 	 */
 	public static function init() {
 
+		add_shortcode( 'gutenworld', array( __CLASS__, 'shortcode' ) );
+
 		// Only proceed if we have Gutenberg ...
 		if ( function_exists( 'register_block_type' ) ) {
 
@@ -66,8 +68,23 @@ class GutenWorld {
 				'gutenworld',
 				'gutenworld',
 				array(
+					// gutenworld/hi
 					'edit_content' => __( 'This is the GutenWorld example block.', 'gutenworld' ),
-					'save_content' => __( 'Hello World! Hello Gutenberg!', 'gutenworld' )
+					'save_content' => __( 'Hello World! Hello Gutenberg!', 'gutenworld' ),
+					// gutenworld/saywhat
+					'say_label' => __( 'Say what?', 'gutenworld' ),
+					'say_placeholder' => __( 'What can I say ... ?', 'gutenworld' ),
+					// gutenworld/shortcode
+					'header_tag_label' => __( 'Header Tag', 'gutenworld' ),
+					'header_tag_placeholder' => __( 'Enter the header tag to use (h1 ... h6 or heading).', 'gutenworld' ),
+					'content_tag_label' => __( 'Content Tag', 'gutenworld' ),
+					'content_tag_placeholder' => __( 'Enter the content tag to use (p,span or div).', 'gutenworld' ),
+					'editing_note' => __( 'Editing block settings &hellip;', 'gutenworld' ),
+					// keywords / aliases to help find the blocks (using the same on all) - the handbook says you can only add three https://wordpress.org/gutenberg/handbook/block-api/
+					'keyword_hello' => __( 'hello', 'gutenworld' ),
+					'keyword_example' => __( 'example', 'gutenworld' ),
+					'keyword_tutorial' => __( 'tutorial', 'gutenworld' ),
+					'shortcode_content_placeholder' => __( 'Optional content rendered below the heading', 'gutenworld' )
 				)
 			);
 
@@ -87,14 +104,127 @@ class GutenWorld {
 				GUTENWORLD_VERSION
 			);
 
-			// Our block type.
-			register_block_type( 'gutenworld/hi', array(
-				'editor_script' => 'gutenworld',
-				'editor_style'  => 'gutenworld-editor',
-				'style'         => 'gutenworld'
-			) );
+			// Our block type named gutenworld/hi
+			register_block_type(
+				'gutenworld/hi',
+				array(
+					'editor_script' => 'gutenworld',
+					'editor_style'  => 'gutenworld-editor',
+					'style'         => 'gutenworld'
+				)
+			);
 
+			// Our block type named gutenworld/hey
+			register_block_type(
+				'gutenworld/hey',
+				array(
+					'editor_script' => 'gutenworld',
+					'editor_style'  => 'gutenworld-editor',
+					'style'         => 'gutenworld'
+				)
+			);
+
+			// Our block type named gutenworld/saywhat
+			register_block_type(
+				'gutenworld/saywhat',
+				array(
+					'editor_script' => 'gutenworld',
+					'editor_style'  => 'gutenworld-editor',
+					'style'         => 'gutenworld'
+				)
+			);
+
+			// This block uses our shortcode
+			register_block_type(
+				'gutenworld/shortcode',
+				array(
+					'editor_script' => 'gutenworld',
+					'editor_style'  => 'gutenworld-editor',
+					'style'         => 'gutenworld',
+					'attributes' => array(
+						'header_tag' => array(
+							'type' => 'string',
+							'default' => 'h2'
+						),
+						'content_tag' => array(
+							'type' => 'string',
+							'default' => 'p'
+						),
+						'content' => array(
+							'type' => 'string',
+							'default' => ''
+						)
+					),
+					'render_callback' => array( __CLASS__, 'shortcode' ),
+				)
+			);
 		}
+	}
+
+	/**
+	 * The [gutenworld] shortcode handler.
+	 *
+	 * @param array $atts shortcode attributes, allowed keys and values: header_tag: h1...h6 and heading, content_tag: p, span or div
+	 * @param string $content enclosed content
+	 *
+	 * @return string output
+	 */
+	public static function shortcode( $atts, $content = '' ) {
+
+		$defaults = array(
+			'header_tag'  => 'h2',
+			'content_tag' => 'p',
+			'content'     => ''
+		);
+
+		$params = shortcode_atts(
+			$defaults,
+			$atts
+		);
+
+		// Take the content from the block if it isn't empty.
+		if ( strlen( $params['content'] ) > 0 && strlen( $content ) === 0 ) {
+			$content = $params['content'];
+		}
+
+		foreach ( $params as $key => $value ) {
+			switch( $key ) {
+				case 'header_tag' :
+					switch ( $value ) {
+						case 'h1' :
+						case 'h2' :
+						case 'h3' :
+						case 'h4' :
+						case 'h5' :
+						case 'h6' :
+						case 'heading' :
+							break;
+						default :
+							$value = $defaults[$key];
+					}
+					break;
+				case 'content_tag' :
+					switch ( $value ) {
+						case 'p' :
+						case 'span' :
+						case 'div' :
+							break;
+						default :
+							$value = $defaults[$key];
+					}
+					break;
+			}
+			$params[$key] = $value;
+		}
+
+		$output = '<' . esc_attr( $params['header_tag'] ) . '>';
+		$output .= esc_html__( 'Hello World! Hello Gutenberg!', 'gutenworld' );
+		$output .= '</' . esc_attr( $params['header_tag'] ) . '>';
+		$output .= '<' . esc_attr( $params['content_tag'] ) . '>';
+		$output .= esc_html( $content );
+		$output .= '</' . esc_attr( $params['content_tag'] ) . '>';
+
+		return $output;
 	}
 }
 
--- js/gutenworld.js
+++ js/gutenworld.js
@@ -20,12 +20,16 @@
 
 if ( typeof wp !== 'undefined' ) {
 
+	//
+	// A fixed block.
+	//
 	wp.blocks.registerBlockType(
 		'gutenworld/hi',
 		{
 			title    : 'GutenWorld',
 			icon     : 'universal-access-alt',
 			category : 'layout',
+			keywords    : [ gutenworld.keyword_example, gutenworld.keyword_tutorial, gutenworld.keyword_hello ],
 			// renders the block in the editor
 			edit     : function( props ) {
 				return wp.element.createElement(
@@ -58,4 +62,217 @@ if ( typeof wp !== 'undefined' ) {
 			}
 		}
 	);
+
+	//
+	// A block where you can input some text.
+	//
+	wp.blocks.registerBlockType(
+		'gutenworld/hey',
+		{
+			title      : 'GutenWorld Hey',
+			icon       : 'universal-access-alt',
+			category   : 'layout',
+			attributes : {
+				content : {
+					type     : 'array',
+					source   : 'children',
+					selector : 'p'
+				}
+			},
+			// renders the block in the editor
+			edit : function( props ) {
+				function onChangeContent( newContent ) {
+					props.setAttributes( { content : newContent } );
+				}
+				return wp.element.createElement(
+					wp.editor.RichText,
+					{
+						tagName   : 'p',
+						className : props.className,
+						onChange  : onChangeContent,
+						value     : props.attributes.content
+					}
+				);
+			},
+			// renders the block on the front end
+			save : function( props ) {
+				return wp.element.createElement(
+					wp.editor.RichText.Content,
+					{
+						tagName   : 'p',
+						className : props.className,
+						value     : props.attributes.content
+					}
+				);
+			}
+		}
+	);
+
+	//
+	// A block with a text input field.
+	//
+	wp.blocks.registerBlockType(
+		'gutenworld/saywhat',
+		{
+			title       : 'GutenWorld Say What?',
+			description : 'A block with a text input field.',
+			icon        : 'universal-access-alt',
+			category    : 'widgets',
+			keywords    : [ gutenworld.keyword_example, gutenworld.keyword_tutorial, gutenworld.keyword_hello ],
+			supports    : { html : true },
+			attributes  : {
+				say : {
+					type : 'string'
+				}
+			},
+			edit : function( props ) {
+				var say = props.attributes.say || '',
+					fields = [];
+				// A text input field.
+				fields.push(
+					wp.element.createElement(
+						wp.components.TextControl,
+						{
+							label : gutenworld.say,
+							value : say,
+							onChange : function( value ) {
+								props.setAttributes( {
+									say : value
+								} );
+							},
+							placeholder : gutenworld.say_placeholder
+						}
+					)
+				);
+				return fields;
+			},
+			// renders the block on the front end
+			save : function( props ) {
+				return wp.element.createElement(
+					'quote',
+					{
+						style : {
+							backgroundColor: '#ccc',
+							color: '#333',
+							padding: '20px'
+						},
+						className : props.className
+					},
+					props.attributes.say
+				);
+			}
+		}
+	);
+
+	//
+	// A block that renders via a PHP callback.
+	// It also uses the ServerSideRender to preview the block in the editor.
+	//
+	wp.blocks.registerBlockType(
+		'gutenworld/shortcode',
+		{
+			title       : 'GutenWorld Shortcode',
+			description : 'Testing rendering by PHP',
+			icon        : 'universal-access-alt',
+			category    : 'widgets',
+			keywords    : [ gutenworld.keyword_example, gutenworld.keyword_tutorial, gutenworld.keyword_hello ],
+			supports    : { html : false },
+			attributes  : {
+				header_tag : {
+					type : 'string'
+				},
+				content_tag : {
+					type : 'string'
+				},
+				content : {
+					type : 'string'
+				}
+			},
+			// Add some inspector controls, use ServerSideRender to preview the block.
+			edit : function( props ) {
+				var header_tag = props.attributes.header_tag || 'h2',
+					content_tag = props.attributes.content_tag || 'p',
+					fields = [];
+
+					fields = [
+						// render the content via our PHP callback
+						wp.element.createElement(
+							wp.components.ServerSideRender,
+							{
+								block : 'gutenworld/shortcode',
+								attributes : props.attributes
+							}
+						),
+						// add the two text input fields as inspector controls -> shown in the block's settings
+						wp.element.createElement(
+							wp.editor.InspectorControls,
+							{ key : 'inspector' },
+							wp.element.createElement(
+								wp.components.TextControl,
+								{
+									label : gutenworld.header_tag_label,
+									value : header_tag,
+									onChange : function( value ) {
+										props.setAttributes( {
+											header_tag : value
+										} );
+									},
+									placeholder : gutenworld.header_tag_placeholder
+								}
+							),
+							wp.element.createElement(
+								wp.components.TextControl,
+								{
+									label : gutenworld.content_tag_label,
+									value : content_tag,
+									onChange : function( value ) {
+										props.setAttributes( {
+											content_tag : value
+										} );
+									},
+									placeholder : gutenworld.content_tag_placeholder
+								}
+							)
+						),
+						// Add the editable content as RichText field.
+						// When text is entered, it will be rendered above the RichText, too,
+						// so it will look strange in the editor but it serves our purpose
+						// as an example of what you could do.
+						wp.element.createElement(
+							wp.editor.RichText,
+							{
+								format : 'string',
+								className : props.className,
+								onChange : function( value ) {
+									props.setAttributes( { content : value } );
+								},
+								value : props.attributes.content,
+								placeholder : gutenworld.shortcode_content_placeholder
+							}
+						)
+					];
+					// Example of acting when the block is selected in the editor, we'll just add a note to illustrate it.
+					if ( props.isSelected ) {
+						fields.push(
+							wp.element.createElement(
+								'div',
+								{style : {
+									backgroundColor: '#eee',
+									color: '#999',
+									padding: '2px',
+									fontSize : '9px'
+								}},
+								gutenworld.editing_note
+							)
+						);
+					}
+				return fields;
+			},
+			// It's rendered via our PHP callback so this returns simply null.
+			save : function( props ) {
+				return null;
+			}
+		}
+	);
+
 }
